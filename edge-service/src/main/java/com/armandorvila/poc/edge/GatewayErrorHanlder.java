40d186c5a993de8585d907ff105ae31a7d144b91
armandorv <armando.ramirezvila@kpn.com>
2018-03-24 19:42:35 +0100

    Added 502 error handling

--- edge-service/src/main/java/com/armandorvila/poc/edge/GatewayErrorHanlder.java
+++ edge-service/src/main/java/com/armandorvila/poc/edge/GatewayErrorHanlder.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
 @Component
-public class GatewayErrorsAttributes extends DefaultErrorAttributes {
+public class GatewayErrorHanlder extends DefaultErrorAttributes {
 
 	@Override
 	public Map<String, Object> getErrorAttributes(ServerRequest request,
