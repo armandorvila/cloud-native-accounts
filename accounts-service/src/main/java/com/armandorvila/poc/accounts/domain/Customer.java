3a86e23a43a14a69e95eb38d0a37b32713cb19c3
armandorv <armando.ramirezvila@kpn.com>
2018-03-25 08:58:32 +0200

    Added customer domain object

--- accounts-service/src/main/java/com/armandorvila/poc/accounts/domain/Customer.java
+++ accounts-service/src/main/java/com/armandorvila/poc/accounts/domain/Customer.java
@@ -2,11 +2,13 @@ package com.armandorvila.poc.accounts.domain;
 
 import java.time.Instant;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -20,14 +22,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = {"createdAt", "lastModifiedAt"})
-@Document(collection = "accounts")
-public class Account {
-	
+@Document(collection = "customers")
+public class Customer {
+
 	@Id
 	private String id;
 	
+	@Email
+	@Indexed(unique = true)
+	private String email;
+	
 	@NotEmpty(message = "This field is required")
-	private String description;
+	private String firstName;
+	
+	@NotEmpty(message = "This field is required")
+	private String lastName; 
 	
 	@CreatedDate
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
@@ -36,13 +45,4 @@ public class Account {
 	@LastModifiedDate
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	private Instant lastModifiedAt;
-	
-	public Account(String description) {
-		this.description = description;
-	}
-	
-	public Account(String id, String description) {
-		this.id = id;
-		this.description = description;
-	}
 }
